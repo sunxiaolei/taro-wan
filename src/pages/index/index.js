75061a13ff01f87f5784b36fe08a4ecb667569a5
sunxiaolei <xiaoleisun92@gmail.com>
2019-02-25 19:52:04 +0800

    create

---
+++ src/pages/index/index.js
@@ -0,0 +1,58 @@
+import Taro, {Component} from '@tarojs/taro'
+import {View, Text, Swiper, SwiperItem} from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  /**
+   * 指定config的类型声明为: Taro.Config
+   *
+   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
+   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
+   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
+   */
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillMount() {
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  render() {
+    return (
+      <View className='index'>
+        <Swiper
+          indicatorColor='#fff'>
+          <SwiperItem>
+            <View className='swiper-item'>
+              <Text className='swiper-text'>A</Text>
+            </View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='swiper-item'>
+              <Text className='swiper-text'>B</Text>
+            </View>
+          </SwiperItem>
+          <SwiperItem>
+            <View className='swiper-item'>
+              <Text className='swiper-text'>C</Text>
+            </View>
+          </SwiperItem>
+        </Swiper>
+      </View>
+    )
+  }
+}
+
